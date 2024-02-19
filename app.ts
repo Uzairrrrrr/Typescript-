bbe5d2e16064ce56ce3c14b95e335c7b840aebf5
uzairrrrrr <uzairawan156@gmail.com>
2024-02-19 07:59:51 +0500

    assigment question

--- app.ts
+++ app.ts
@@ -1,3 +1,97 @@
-let message = "Hello World"
-console.log(message)
+// let message = "Hello World"
+// console.log(message) 
+
+// let message : String = "Hello Eric, would you like to learn some Python today?"
+// console.log(message)
+
+// let message : string = 'eric no'
+// console.log("UpperCase: " +message.toUpperCase())  
+// console.log("LowerCase: "+message.toLowerCase())
+// console.log("Titlecase: " + toTitleCase(message));
+
+// function toTitleCase(str: string): string {
+//     return str.replace(/\w\S*/g, function(txt) {
+//         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+//     });
+// }
+
+// const quote: string = "Any fool can write code that a computer can understand. Good programmers write code that humans can understand."
+// const author: string = "Larry Wall"
+
+// console.log(`${author} once said, "${quote}"`)
+
+// const quote: string = "Any fool can write code that a computer can understand. Good programmers write code that humans can understand."
+// const author: string = "Larry Wall"
+// const message: string = `${author} once said, "${quote}"`
+// console.log(message)
+
+// const nameWithWhitespace: string = "\t\n   Uzair \t\n";
+
+// console.log("Name with whitespace:");
+// console.log(nameWithWhitespace);
+
+// const nameStripped: string = nameWithWhitespace.trim();
+
+// console.log("\nName stripped of whitespace:");
+// console.log(nameStripped);
+
+// console.log("Addition: " + (5 + 3))
+
+// console.log("Subtraction: " + (10 - 2))
+
+// console.log("Multiplication: " + (4 * 2))
+
+// console.log("Division: " + (16 / 2))
+
+// console.log(5 + 3);
+// console.log(10 - 2);
+// console.log(4 * 2);
+// console.log(16 / 2);
+
+// const FavNumber: number = 7
+// const message: string = `My favorite number is ${FavNumber}`;
+// console.log(message)
+
+// const FavNumber: number = 7
+// const message: string = `My favorite number is ${FavNumber}`;
+// This is the program where i store my fav  number in a variable and then use it to create a message that says "my favourite number is 7
+// console.log(message)
+
+// const names: string[] = ['saad', 'ahmed', 'sameer']
+// for(let i = 0; i < names.length; i++){
+//     console.log(names[i])
+// }
+
+// const names: string[] = ['saad', 'ahmed', 'sameer']
+// for(let i = 0; i < names.length; i++){
+//     console.log(`Hey there, ${names[i]}!`)
+// }
+
+// const favVehicle: string[] = ['car', 'motorcycle', 'scooter']
+// for(let i = 0; i < favVehicle.length; i++){
+//     console.log(`I would like to own a ${favVehicle[i]}.`)
+// }
+
+// const guestList: string[] = ["Faizan", "Ali", "Owais"];
+
+// guestList.forEach(person => {
+//     console.log(`Dear ${person}, I would be honored to have you join me for dinner.`);
+// });
+
+// const guestList: string[] = ["Faizan", "Ali", "Owais"];
+
+// guestList.forEach(person => {
+//     console.log(`Dear ${person}, I would be honored to have you join me for dinner.`);
+// });
+// const personnotcomming : string = "Ali";
+// console.log(`${personnotcomming} is not comming`);
+// const newguest: string = "Ahmed"
+// const index = guestList.indexOf(personnotcomming);
+// if (index !== -1){
+//     guestList[index] = newguest;
+// }
+// guestList.forEach(person => {
+//     console.log(`Dear ${person}, I would be honored to have you join me for dinner.`);
+// });
+
 
